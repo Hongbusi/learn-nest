18d16da816eb68e90cf8e68d112120738a48f630
Hongbusi <coderhbs@gmail.com>
2023-04-19 14:17:21 +0800

    chore: config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,19 @@
 import type { MiddlewareConsumer, NestModule } from '@nestjs/common'
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { CatsModule } from './cats/cats.module'
 import { LoggerMiddleware } from './common/middleware/logger.middleware'
+import configuration from './config/configuration'
 
 @Module({
-  imports: [CatsModule],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+    }),
+    CatsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
