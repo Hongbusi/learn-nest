4a3acfaf90a2f098a4e42096c18dc1d712ae8395
Hongbusi <coderhbs@gmail.com>
2023-04-20 18:54:48 +0800

    chore: open api

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.setGlobalPrefix('/api/v1')
+
+  const config = new DocumentBuilder()
+    .setTitle('User example')
+    .setDescription('The user API description')
+    .setVersion('1.0')
+    .addTag('user')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
+
   await app.listen(3000)
 }
 
