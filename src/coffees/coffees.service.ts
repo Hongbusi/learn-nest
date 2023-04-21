42ba8e36532ec3486b92b589bcfb4e36078d7991
Hongbusi <coderhbs@gmail.com>
2023-04-21 16:30:42 +0800

    chore: retrieve entities with their relations

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -13,11 +13,16 @@ export class CoffeesService {
   ) {}
 
   findAll() {
-    return this.coffeeRepository.find()
+    return this.coffeeRepository.find({
+      relations: ['flavors'],
+    })
   }
 
   async findOne(id: string) {
-    const coffee = await this.coffeeRepository.findOneBy({ id: +id })
+    const coffee = await this.coffeeRepository.findOne({
+      where: { id: +id },
+      relations: ['flavors'],
+    })
     if (!coffee) {
       throw new NotFoundException(`Coffee #${id} not found`)
     }
