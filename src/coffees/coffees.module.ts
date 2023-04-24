ccc843655f0b04e4dff765efc176b2df7c3781d3
Hongbusi <coderhbs@gmail.com>
2023-04-24 23:01:36 +0800

    chore: non-class-based provider tokens

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -4,13 +4,14 @@ import { CoffeesController } from './coffees.controller'
 import { CoffeesService } from './coffees.service'
 import { Coffee } from './entities/coffee.entity'
 import { Flavor } from './entities/flavor.entity'
-
-class MockCoffeesService {}
+import { COFFEE_BRANDS } from './coffees.constants'
 
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
-  providers: [{ provide: CoffeesService, useValue: new MockCoffeesService() }],
-  exports: [CoffeesService]
+  providers: [
+    CoffeesService,
+    { provide: COFFEE_BRANDS, useValue: ['buddy brew', 'nescafe'] }],
+  exports: [CoffeesService],
 })
 export class CoffeesModule {}
