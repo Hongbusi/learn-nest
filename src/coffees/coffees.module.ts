82eccf013e8dca816b188fe50684ba9245ea2212
Hongbusi <coderhbs@gmail.com>
2023-04-24 23:09:45 +0800

    chore: factory providers

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common'
+import { Injectable, Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { CoffeesController } from './coffees.controller'
 import { CoffeesService } from './coffees.service'
@@ -10,16 +10,28 @@ class ConfigService {}
 class DevelopmentConfigService {}
 class ProductionConfigService {}
 
+@Injectable()
+export class CoffeeBrandsFactory {
+  create() {
+    return ['buddy brew', 'nescafe']
+  }
+}
+
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
   providers: [
     CoffeesService,
+    CoffeeBrandsFactory,
     {
       provide: ConfigService,
       useClass: process.env.NODE_ENV === 'development' ? DevelopmentConfigService : ProductionConfigService,
     },
-    { provide: COFFEE_BRANDS, useValue: ['buddy brew', 'nescafe'] }],
+    {
+      provide: COFFEE_BRANDS,
+      useFactory: (brandsFactory: CoffeeBrandsFactory) => brandsFactory.create(),
+      inject: [CoffeeBrandsFactory],
+    }],
   exports: [CoffeesService],
 })
 export class CoffeesModule {}
