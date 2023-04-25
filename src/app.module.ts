cfa78066d40286abf4ad3fa30e96e507177f56bf
Hongbusi <coderhbs@gmail.com>
2023-04-25 23:23:52 +0800

    chore: asynchronously configure dynamic modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,6 +11,18 @@ import appConfig from './config/app.config'
 
 @Module({
   imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DATABASE_HOST,
+        port: +process.env.DATABASE_PORT,
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
     ConfigModule.forRoot({
       validationSchema: Joi.object({
         DATABASE_HOST: Joi.required(),
@@ -19,16 +31,7 @@ import appConfig from './config/app.config'
       load: [appConfig],
     }),
     CoffeesModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: +process.env.DATABASE_PORT,
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
+
     CoffeeRatingModule,
     DatabaseModule,
   ],
