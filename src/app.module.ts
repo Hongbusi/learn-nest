c7251ec7247acae386998587bb2861d71a1bc32f
Hongbusi <coderhbs@gmail.com>
2023-04-25 22:56:29 +0800

    chore: schema validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { ConfigModule } from '@nestjs/config'
+import * as Joi from '@hapi/joi'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { CoffeesModule } from './coffees/coffees.module'
@@ -9,7 +10,12 @@ import { DatabaseModule } from './database/database.module'
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        DATABASE_HOST: Joi.required(),
+        DATABASE_PORT: Joi.number().default(5432),
+      }),
+    }),
     CoffeesModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
