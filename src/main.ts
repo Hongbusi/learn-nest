637a5b5f2a20be18db09e3bac46297fdb68b222a
Hongbusi <coderhbs@gmail.com>
2023-04-26 16:06:24 +0800

    chore: support hmr

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core'
 import { ValidationPipe } from '@nestjs/common'
 import { AppModule } from './app.module'
 
+declare const module: any
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.useGlobalPipes(
@@ -15,5 +17,10 @@ async function bootstrap() {
     }),
   )
   await app.listen(3000)
+
+  if (module.hot) {
+    module.hot.accept()
+    module.hot.dispose(() => app.close())
+  }
 }
 bootstrap()
