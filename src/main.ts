802da01ffbed2dce62f3be87308943958f6c96f3
Hongbusi <coderhbs@gmail.com>
2023-04-29 23:22:47 +0800

    chore: handing timeouts with interceptors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module'
 import { setupSwagger } from './swagger'
 import { HttpExceptionFilter } from './common/filters/http-exception/http-exception.filter'
 import { WrapResponseInterceptor } from './common/interceptors/wrap-response/wrap-response.interceptor'
+import { TimeoutInterceptor } from './common/interceptors/timeout/timeout.interceptor'
 
 declare const module: any
 
@@ -20,7 +21,10 @@ async function bootstrap() {
     }),
   )
   app.useGlobalFilters(new HttpExceptionFilter())
-  app.useGlobalInterceptors(new WrapResponseInterceptor())
+  app.useGlobalInterceptors(
+    new WrapResponseInterceptor(),
+    new TimeoutInterceptor(),
+  )
 
   const enableSwagger = process.env.ENABLE_DOCUMENTATION !== 'false'
 
