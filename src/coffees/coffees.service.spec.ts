4d85b7847d44a2e2b874ff8f3348d089ebf97717
Hongbusi <coderhbs@gmail.com>
2023-04-30 20:34:46 +0800

    chore: getting started with test suites

--- src/coffees/coffees.service.spec.ts
+++ src/coffees/coffees.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing'
+import { Connection } from 'typeorm'
+import { getRepositoryToken } from '@nestjs/typeorm'
 import { CoffeesService } from './coffees.service'
+import { Coffee } from './entities/coffee.entity'
+import { Flavor } from './entities/flavor.entity'
 
 describe('CoffeesService', () => {
   let service: CoffeesService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CoffeesService],
+      providers: [
+        CoffeesService,
+        { provide: Connection, useValue: {} },
+        { provide: getRepositoryToken(Flavor), useValue: {} },
+        { provide: getRepositoryToken(Coffee), useValue: {} },
+      ],
     }).compile()
 
     service = module.get<CoffeesService>(CoffeesService)
