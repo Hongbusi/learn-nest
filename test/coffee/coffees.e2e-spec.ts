f0c176f44fa93fae4fe0330282ba6737b5644711
Hongbusi <coderhbs@gmail.com>
2023-04-30 21:35:17 +0800

    chore: implementing e2e test logic

--- test/coffee/coffees.e2e-spec.ts
+++ test/coffee/coffees.e2e-spec.ts
@@ -1,9 +1,17 @@
-import { INestApplication } from '@nestjs/common'
+import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common'
 import { Test, TestingModule } from '@nestjs/testing'
 import { TypeOrmModule } from '@nestjs/typeorm'
+import * as request from 'supertest'
 import { CoffeesModule } from 'src/coffees/coffees.module'
+import { CreateCoffeeDto } from 'src/coffees/dto/create-coffee.dto'
 
 describe('[Feature] Coffees - /coffees', () => {
+  const coffee = {
+    name: 'Shipwreck Roast',
+    brand: 'Buddy Brew',
+    flavors: ['chocolate', 'vanilla'],
+  }
+
   let app: INestApplication
 
   beforeAll(async () => {
@@ -23,8 +31,38 @@ describe('[Feature] Coffees - /coffees', () => {
     }).compile()
 
     app = moduleFixture.createNestApplication()
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        transform: true,
+        forbidNonWhitelisted: true,
+        transformOptions: {
+          enableImplicitConversion: true,
+        },
+      }),
+    )
     await app.init()
   })
+
+  it.todo('Create [POST /]', () => {
+    return request(app.getHttpServer())
+      .post('/coffees')
+      .send(coffee as CreateCoffeeDto)
+      .expect(HttpStatus.CREATED)
+      .then(({ body }) => {
+        const expectedCoffee = jasmine.objectContaining({
+          ...coffee,
+          flavors: jasmine.arrayContaining(
+            coffee.flavors.map(name => jasmine.objectContaining({ name })),
+          ),
+        })
+        expect(body).toEqual(expectedCoffee)
+      })
+  })
+  it.todo('Get all [GET /]')
+  it.todo('Get one [GET /:id]')
+  it.todo('Update [PATCH /:id]')
+  it.todo('Delete [DELETE /:id]')
 
   afterAll(async () => {
     await app.close()
