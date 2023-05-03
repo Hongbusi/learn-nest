05381189f9a9b85f71aa8ac4ac5932a564f8fd1f
Hongbusi <coderhbs@gmail.com>
2023-05-03 14:00:35 +0800

    chore: protecting our routes with a guard

--- src/iam/iam.module.ts
+++ src/iam/iam.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { JwtModule } from '@nestjs/jwt'
 import { ConfigModule } from '@nestjs/config'
+import { APP_GUARD } from '@nestjs/core'
 import { User } from '../users/entities/user.entity'
 import jwtConfig from '../config/jwt.config'
 import { HashingService } from './hashing/hashing.service'
 import { BcryptService } from './hashing/bcrypt.service'
 import { AuthenticationController } from './authentication/authentication.controller'
 import { AuthenticationService } from './authentication/authentication.service'
+import { AccessTokenGuard } from './authentication/guards/access-token.guard'
 
 @Module({
   imports: [
@@ -19,6 +21,10 @@ import { AuthenticationService } from './authentication/authentication.service'
     {
       provide: HashingService,
       useClass: BcryptService,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: AccessTokenGuard,
     },
     AuthenticationService,
   ],
